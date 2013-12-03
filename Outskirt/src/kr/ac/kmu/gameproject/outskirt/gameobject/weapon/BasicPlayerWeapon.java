444c2df4734e8bc64ad2ff98624ac0ccb6ac5192
Nevermethion <none>
2013-12-02 16:47:19 -0800

    add fichier manquant

---
+++ Outskirt/src/kr/ac/kmu/gameproject/outskirt/gameobject/weapon/BasicPlayerWeapon.java
@@ -0,0 +1,27 @@
+package kr.ac.kmu.gameproject.outskirt.gameobject.weapon;
+
+import kr.ac.kmu.gameproject.outskirt.GameObject;
+import kr.ac.kmu.gameproject.outskirt.gameobject.SpaceSheep;
+import kr.ac.kmu.gameproject.outskirt.screen.Game;
+
+public class BasicPlayerWeapon extends BasicWeapon {
+	
+	public BasicPlayerWeapon(Game game, SpaceSheep owner, Game.Color color) {
+		super(game, owner, color);
+	}
+
+	public void draw() {
+		if (game.getApp().isPressed('q')) {
+			color = Game.Color.GREEN;
+			((SpaceSheep) owner).setGreen();
+		}
+		if (game.getApp().isPressed('w')) {
+			color = Game.Color.RED;
+			((SpaceSheep) owner).setRed();
+		}
+		if (game.getApp().isPressed('e')) {
+			color = Game.Color.CYAN;
+			((SpaceSheep) owner).setBlue();
+		}
+	}
+}
