54ace3d38a5c4ace3839af766eadf962b9258886
Alexis Brissard <alexis.brissard@epitech.eu>
2013-12-05 11:33:48 +0900

    Add powerful timer !

--- Outskirt/src/kr/ac/kmu/gameproject/outskirt/Timer.java
+++ Outskirt/src/kr/ac/kmu/gameproject/outskirt/Timer.java
@@ -2,24 +2,50 @@ package kr.ac.kmu.gameproject.outskirt;
 
 public class Timer {
     
-    private long startTime = 0;
+    private long totalTime = 0;
+    private long previousUpdateTime = 0;
+    private long elapsedTime = 0;
     private long updateTime = 0;
+    private boolean running;
 
+    public Timer(boolean running) {
+    	this.running = running;
+    }
+    
     public Timer() {
-        startTime = System.currentTimeMillis();
-        updateTime = startTime;
+    	this(true);
     }
 
     public long getTotalTime() {
-		return System.currentTimeMillis() - startTime;
+		return totalTime;
     }
     
     public void updateTime() {
-    	updateTime = System.currentTimeMillis();
+    	if (running) {
+	    	previousUpdateTime = updateTime;
+	    	updateTime = System.currentTimeMillis();
+	    	elapsedTime = previousUpdateTime - updateTime;
+	    	totalTime += elapsedTime;
+    	}
     }
 
     public long getElapsedTime() {
-    	return System.currentTimeMillis() - updateTime;
+    	return elapsedTime;
     }
 
+    public void start() {
+    	running = true;
+    }
+    
+    public void stop() {
+    	running = false;
+    }
+    
+    public void reset() {
+        totalTime = 0;
+        previousUpdateTime = 0;
+        elapsedTime = 0;
+        updateTime = 0;
+    }
+    
 }
